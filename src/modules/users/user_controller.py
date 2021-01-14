7769b3fb8f6ce7e5aa811cad0773877bdc774c65
Stiven Rodriguez <judrobinx@gmail.com>
2021-01-13 20:09:23 -0500

    refactor (users): use blueprint for controllers

---
+++ src/modules/users/user_controller.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, request, jsonify
+from modules.users import userService
+from common.exceptions import InternalServerError
+
+Users = Blueprint('users', __name__)
+
+@Users.route('/users')
+def get_users():
+  response = userService.get_users()
+  return jsonify(response)
+
+@Users.route('/users/<id>')
+def find_user(id):
+  response = userService.find_user(id)
+  return jsonify(response)
+
+@Users.route('/users', methods=['POST'])
+def create_user(self):
+  data = request.json
+  response = userService.create_user(data)
+  return jsonify(response)
+
+@Users.route('/users', methods=['PUT'])
+def update_user(self):
+  data = request.json
+  response = userService.update_user(data)
+  return jsonify({"message": "User updated"})
+
+@Users.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+  response = userService.find_user(id)
+  return jsonify({"message": "User deleted"})
